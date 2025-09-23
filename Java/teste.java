79780f74e4c586a72d41a4f241279165042b72e5
Álvaro Lúcio <alvarolucio2007@gmail.com>
2025-09-23 07:03:35 -0300

    .

--- Java/teste.java
+++ Java/teste.java
@@ -1,8 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Everyone!");
-        long a = Long.MAX_VALUE;
-        System.out.println("Long max value = "+ a);
-
+        long a = Long.MAX_VALUE+2  ;
+        System.out.println(a);
+        
     }
 }
