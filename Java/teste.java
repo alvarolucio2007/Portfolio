0e39aa2f5fe61fa07726fc671e2164ff3bd0987d
Álvaro Lúcio <alvarolucio2007@gmail.com>
2025-09-23 06:54:03 -0300

    prática.

--- Java/teste.java
+++ Java/teste.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Everyone!");
+        long a = Long.MAX_VALUE;
+        System.out.println("Long max value = "+ a);
+
     }
 }
