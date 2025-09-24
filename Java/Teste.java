6b32eb64c1ed1694bfc0198dbbc1fbc0460aabd9
Álvaro Lúcio <alvarolucio2007@gmail.com>
2025-09-24 20:39:59 -0300

    mais pratica.

--- Java/Teste.java
+++ Java/Teste.java
@@ -1,7 +1,8 @@
 public class Teste {
 
     public static void main(String[] args) {
-        int a = Integer.MIN_VALUE-1;
-        System.out.println(a);
+        byte teste1=1;short teste2=2;int teste3=3;
+        short ajuda = (short) (10*(teste1+teste2+teste3));
+        System.out.println(ajuda);
     }
 }
