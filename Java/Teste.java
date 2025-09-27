408b2ec710b2d8b9e9658349d96468f09427f77e
Álvaro Lúcio <alvarolucio2007@gmail.com>
2025-09-26 23:33:02 -0300

    .

--- Java/Teste.java
+++ Java/Teste.java
@@ -1,8 +1,6 @@
 public class Teste {
 
     public static void main(String[] args) {
-        byte teste1=1;short teste2=2;int teste3=3;
-        short ajuda = (short) (10*(teste1+teste2+teste3));
-        System.out.println(ajuda);
+       System.out.println("Hello WOrld!");
     }
 }
