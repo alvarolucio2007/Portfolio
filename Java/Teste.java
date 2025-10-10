068d03f091b38b31a06496795f6f2d12291b64ab
Álvaro Lúcio <alvarolucio2007@gmail.com>
2025-10-10 19:46:37 -0300

    .

--- Java/Teste.java
+++ Java/Teste.java
@@ -1,13 +1,5 @@
-public class Teste {
-
+public class Teste{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.flush();
-    }
-}
-
-public class Teste2 {
-    public static void main(String[] args) {
-        System.out.println("Teste");
+        System.out.print("Opa álvaro");
     }
 }
