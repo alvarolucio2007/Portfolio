0f7c4c646db7e9967ef5baeca22b4d3b701aba50
Álvaro Lúcio Mousinho Coelho <alvarolucio2007@proton.me>
2025-12-01 07:17:21 -0300

    conhecendo mais Go.

--- Go/teste.go
+++ Go/teste.go
@@ -1,38 +1,12 @@
 package main
-
-import (
-	"errors"
-	"fmt"
-)
-
-
-func obterDia(numero int) (string,error){
-	var dia string
-	switch numero{
-		case 1:
-			dia="Domingo"
-		case 2:
-			dia="Segunda-feira"
-		case 3:
-			dia="Terça-feira"
-		case 4:
-			dia="Quarta-feira"
-		case 5:
-			dia="Quinta-feira"
-		case 6:
-			dia="Sexta-feira"
-		case 7:
-			dia="Sábado"
-		default:
-			return "",errors.New("Erro, número de dia inválido! Por favor, tente novamente com números inteiros entre 1 e 7.")
-}
-	return dia,nil
-}
+import ("fmt")
 func main() {
-	dia_1,erro_1:=obterDia(4)
-	if erro_1!= nil{
-		fmt.Println("Erro!",erro_1)
+	fmt.Println("Hello World!")
+	var a int
+	a=0
+	if a==0{
+		fmt.Println(a)
 	} else{
-		fmt.Printf("O número é: %s\n", dia_1)
+		fmt.Println("Pode não mn")
 	}
 }
